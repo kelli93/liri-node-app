c48113bc621e93462027090679488ae0c981a7b0
Kelli <35212823+kelli93@users.noreply.github.com>
2018-08-02 11:46:02 -0400

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,13 +1,14 @@
 console.log('this is loaded');
 
-exports.twitter = {
+/** exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-};
+};**/
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id:  '5871cb99feef499fab603214a9fc8329',
+  secret: '36cae33346d345a2a23be304b0e8c972'
 };
+
